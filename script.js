c0841d169b028ad337e2ad5011e058b9b0d28ab8
Saleem Malik <77879142+Saleemalik@users.noreply.github.com>
2021-01-30 22:46:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+           $('.header').addClass("sticky"); 
+        }else{
+            $('.header').removeClass("sticky"); 
+        }
+    })
+});
+
+
